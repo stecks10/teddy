376ae1c009d620c576110e49d12feb72380e540a
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-02-01 18:26:37 -0300

    css do toast

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,19 +2,26 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "../hooks/use-toast";
 
 export function Login() {
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
+  const { toast } = useToast();
 
   const handleLogin = () => {
     if (username) {
       localStorage.setItem("username", username);
       navigate("/home");
     } else {
-      alert("Digite o seu nome");
+      toast({
+        title: "Erro",
+        description: "Digite o seu nome",
+        variant: "destructive",
+      });
     }
   };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white">
       <h1 className="text-2xl mb-4">Olá, seja bem-vindo!</h1>
